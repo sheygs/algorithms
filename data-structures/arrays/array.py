50846ace17de0024088d487b1a6ef78f915ede24
Olusegun Ekoh <segun.ekoh@gmail.com>
2025-05-30 21:44:38 +0100

    chore(`arrays`): Implement `insert` and `delete` methods

--- data-structures/arrays/array.py
+++ data-structures/arrays/array.py
@@ -3,22 +3,22 @@
 """
 
 
-class Array:
+class MyArray:
     def __init__(self):
-        self.name = 'array'
         self.length = 0
         self.store = {}
 
-    # Time complexity: o(1)
+    # Time complexity: O(1)
     def get(self):
-        return self.__dict__
+        print('self: ', self.store.values())
+        return str(self.__dict__)
 
-    # Time complexity: o(1)
+    # Time complexity: O(1)
     def append(self, item):
         self.length += 1
         self.store[self.length - 1] = item
 
-    # Time complexity: o(1)
+    # Time complexity: O(1)
     def pop(self):
         if self.length == 0 or not self.store:
             print('array already empty!')
@@ -28,11 +28,35 @@ class Array:
         self.length -= 1
         return removed
 
+    # Time complexity: O(n)
+    def insert(self, item, index: int) -> None:
+        self.length += 1
 
-arr = Array()
-arr.append('1')
-arr.append('2')
-arr.pop()
-arr.pop()
-arr.pop()
-print(f'array: {arr.get()}')
+        if index < 0 or index > self.length - 1:
+            raise ValueError('Invalid index range')
+
+        for i in range(self.length - 1, index, -1):
+            self.store[i] = self.store[i - 1]
+
+        self.store[index] = item
+
+    # Time complexity: O(n)
+    def delete(self, index):
+        if index < 0 or index > self.length - 1:
+            raise ValueError('Invalid index range')
+
+        for i in range(index, self.length - 1):
+            self.store[i] = self.store[i + 1]
+
+        del self.store[index]
+        self.length -= 1
+
+
+if __name__ == "__main__":
+    arr = MyArray()
+    arr.append('one')
+    arr.append('two')
+    arr.append('four')
+    arr.append('five')
+    # arr.delete(3)
+    print(f'array: {arr.get()}')
